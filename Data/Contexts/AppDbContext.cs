80055720d05833dd7e38a2e6801a298f6b3d6238
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-04-11 00:27:17 +0200

    Create database and setup connectionstring

---
+++ Data/Contexts/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+// Chat-GPT4o helped me with the IdentityDbContext<UserEntity> and what it does is that it gives it 
+// automatic access to all tables and functions that is needed to manage users, roles and logins on a correct and secure way
+public class AppDbContext : IdentityDbContext<UserEntity>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<ProjectEntity> Projects { get; set; }
+    public DbSet<ClientEntity> Clients { get; set; }
+
+    //Chat-GPT4o helped me with OnModelCreating, defining a precision and scale for the budget column.
+    //Since i had a warning-message when creating a migration - No store type was specified for the decimal property 'Budget' on entity type 'ProjectEntity'.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ProjectEntity>()
+            .Property(p => p.Budget)
+            .HasColumnType("decimal(18,2)");
+    }
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,5 +1,22 @@
+using Data.Contexts;
+using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddIdentity<UserEntity, IdentityRole>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequiredLength = 8;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 

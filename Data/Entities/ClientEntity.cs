8c132f32876943f810c007c77038d8b49ca025e2
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-04-10 10:37:16 +0200

    Project, Clien & User Entities set up

---
+++ Data/Entities/ClientEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+public class ClientEntity
+{
+    [Key]
+    public string Id { get; set; } = null!;
+    public string ClientName { get; set; } = null!;
+}
---
+++ Data/Entities/ProjectEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities;
+
+public class ProjectEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    [Required]
+    public string ProjectName { get; set; } = null!;
+    public string? Image {  get; set; }
+    [Required]
+    public string Description { get; set; } = null!;
+    [Required]
+    [Column(TypeName = "date")]
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    [Required]
+    public decimal Budget { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    [Required]
+    public ProjectStatus Status { get; set; } = ProjectStatus.Started;
+
+    [ForeignKey(nameof(Client))]
+    public string ClientId { get; set; } = null!;
+    public ClientEntity Client { get; set; } = null!;
+
+    [ForeignKey(nameof(User))]
+    public string UserId { get; set; } = null!;
+    public UserEntity User { get; set; } = null!;
+
+
+
+
+    public enum ProjectStatus
+    {
+        Started,
+        Completed
+    }
+
+}

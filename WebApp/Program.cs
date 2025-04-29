302451b1ae032524aed2a6698306deab9a7ae74a
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-04-30 00:59:47 +0200

    AuthController for Register and Login is setup

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -32,6 +32,14 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -43,6 +51,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHsts();
 app.UseHttpsRedirection();
+app.UseSession();
 app.UseRouting();
 app.UseAuthorization();
 app.MapStaticAssets();
